d3fb4ac7d02389157111703f158a341e5705d2cb
Yiwei Sun <ys2882@columbia.edu>
2016-10-26 17:25:51 -0400

    Add files via upload

--- lib/data_cleaning.R
+++ lib/data_cleaning.R
@@ -2,9 +2,16 @@
 # 
 library(data.table)
 library(dplyr)
+library(rpart)
+library(randomForest)
 setwd("G:/Columbia/study/3rd semester/5243/project3/Project3_poodleKFC_train/")
 dat_train=fread("sift_features.csv")
-label1=data.frame(rep("chicken",1000))
-label2=data.frame(rep("dog",1000))
+label1=data.frame(rep(0,1000))
+label2=data.frame(rep(1,1000))
 label_train=c(t(label1),t(label2))
-
+dat_train=tbl_df(t(dat_train))    # column denotes feature, row denotes pictures
+dat_train=mutate(dat_train,label=label_train) # the last column is label,
+                                              # 0:chicken, 1: dog
+### decision tree part
+tree_fit <- rpart(label~ .,
+             method="class", data=dat_train)
