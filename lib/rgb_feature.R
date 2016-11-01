9a020f2e05bc1075eeaeb290bec5c30db1298af6
Yiwei Sun <ys2882@columbia.edu>
2016-11-01 18:34:25 -0400

    Update rgb_feature.R

--- lib/rgb_feature.R
+++ lib/rgb_feature.R
@@ -1,35 +1,12 @@
-### ATTENTION
-### We have 13 images that cannot be read in R, so delete those images before running this code.
-### Their names are "Abyssinian_34", "Abyssinian_5", "Egyptian_Mau_129", "Egyptian_Mau_139",
-### "Egyptian_Mau_14", "Egyptian_Mau_145", "Egyptian_Mau_156", "Egyptian_Mau_167",
-### "Egyptian_Mau_177", "Egyptian_Mau_186", "Egyptian_Mau_191", "staffordshire_bull_terrier_2", 
-### "staffordshire_bull_terrier_22".
-
+### Extract RGB feature
 library(R.matlab)
-setwd("G:/Columbia/study/3rd semester/5243/project3/Project3_poodleKFC_train/")
+library(dplyr)
+library(randomForest)
+library("e1071")
+setwd("C:/Users/ys2882/Downloads")
 dir_images <- "./images"
 dir_names <- list.files(dir_images)
-
-# breed_name <- rep(NA, length(dir_names))
-# for(i in 1:length(dir_names)){
-#   tt <- unlist(strsplit(dir_names[i], "_"))
-#   tt <- tt[-length(tt)]
-#   breed_name[i] = paste(tt, collapse="_", sep="")
-# }
-# cat_breed <- c("Abyssinian", "Bengal", "Birman", "Bombay", "British_Shorthair", "Egyptian_Mau",
-#                "Maine_Coon", "Persian", "Ragdoll", "Russian_Blue", "Siamese", "Sphynx")
-# 
-# iscat <- breed_name %in% cat_breed
-# y_cat <- as.numeric(iscat)
-# 
-# # I renamed the label "0" to be "-1" just for convenience when using certain algorithms like perceptron.
-# for (i in (1:length(y_cat))) {
-#   if (y_cat[i] == 0) {
-#     y_cat[i] <- -1
-#   }
-# }
-# # this would be train labels.
-# writeMat("y.mat", y_catexport = y_cat)
+label_train=c(t(data.frame(rep(1,1000))),t(data.frame(rep(0,1000))))
 
 ########################################################################## RGB
 
@@ -47,7 +24,7 @@ bBin <- seq(0, 1, length.out=nB)
 
 ## create feature files
 #rgb_feature <- list()
-rgb_feature_all=matrix(data=NA,nrow=length(dir_names),ncol=800)
+rgb_feature_all=matrix(data=NA,nrow=length(dir_names),ncol=nR*nG*nB)
 for (i in (1:length(dir_names))) {
   img <- readImage(paste("./images/", dir_names[i], sep = ""))
   img <- resize(img,250,250)
@@ -61,23 +38,32 @@ for (i in (1:length(dir_names))) {
                                          levels=1:nB)))
   rgb_feature <- as.numeric(freq_rgb$Freq)/(ncol(mat)*nrow(mat))
   rgb_feature_all[i,]=rgb_feature
-  #name <- unlist(strsplit(dir_names[i], "[.]"))[1]
-  # create a new folder named "color_feature", which is in the same directory as images folder.
-  #writeMat(paste("./color_feature/", name, ".mat", sep = ""), rgb_featureexport = rgb_feature)
 }
+save(file="rgb_feature.RData",rgb_feature_all)
+# ### PCA
+# sub_feature=prcomp(rgb_feature_all)
+# sub_feature2=data.frame(sub_feature$x[,1:500])
+# sub_feature3=mutate(sub_feature2,label=as.factor(label_train))
+# 
+# dat_train=mutate(data.frame(rgb_feature_all),label=factor(label_train))
+# randomForest(label~.,data=sub_feature3)
+# svm1=svm(label~.,data=sub_feature3)
+# pred=predict(svm1,sub_feature3[,1:500])
+# table(pred,label_train)
 
 ########################################################################## HSV
-library(grDevices)
+# library(grDevices)
 nH <- 10
 nS <- 6
 nV <- 6
 hBin <- seq(0, 1, length.out=nH)
 sBin <- seq(0, 1, length.out=nS)
 vBin <- seq(0, 0.005, length.out=nV)
+hsv_feature_all=matrix(data=NA,nrow=length(dir_names),ncol=nH*nS*nV)
 
 for (i in (1:length(dir_names))) {
-  print(i)
   img <- readImage(paste("./images/", dir_names[i], sep = ""))
+  img <- resize(img,250,250)
   mat <- imageData(img)
   mat_rgb <- mat
   dim(mat_rgb) <- c(nrow(mat)*ncol(mat), 3)
@@ -86,11 +72,9 @@ for (i in (1:length(dir_names))) {
                                   factor(findInterval(mat_hsv[2,], sBin), levels=1:nS), 
                                   factor(findInterval(mat_hsv[3,], vBin), levels=1:nV)))
   hsv_feature <- as.numeric(freq_hsv$Freq)/(ncol(mat)*nrow(mat)) # normalization
-  name <- unlist(strsplit(dir_names[i], "[.]"))[1]
-  # create a new folder named "HSV_color_feature", which is in the same directory as images folder.
-  writeMat(paste("./HSV_color_feature/", name, ".mat", sep = ""), hsv_featureexport = hsv_feature)
+  hsv_feature_all[i,]=hsv_feature
 }
-
+save(file="hsv_feature.RData",hsv_feature_all)
 
 ########################################################################## Spatial
 
@@ -99,7 +83,7 @@ M <- 5 # number of bins in y-axis
 p_x <- p_y <- 250
 xbin <- floor(seq(0, p_x, length.out= N+1))
 ybin <- seq(0, p_y, length.out=M+1)
-
+spacial_feature_all=matrix(data=NA,nrow=length(dir_names),ncol=12000)
 
 construct_rgb_feature <- function(X){
   freq_rgb <- as.data.frame(table(factor(findInterval(X[,,1], rBin), levels=1:nR), 
@@ -120,10 +104,11 @@ for (i in (1:length(dir_names))) {
       scf[((M*(k-1)+j-1)*nR*nG*nB+1):((M*(k-1)+j)*nR*nG*nB)] <- construct_rgb_feature(tmp) 
     }
   }
-  name <- unlist(strsplit(dir_names[i], "[.]"))[1]
-  # create a new folder named "spatial_color_feature", which is in the same directory as images folder.
-  writeMat(paste("./spatial_color_feature/", name, ".mat", sep = ""), scfexport = scf)
+  spacial_feature_all[i,]=scf
+  # name <- unlist(strsplit(dir_names[i], "[.]"))[1]
+  # # create a new folder named "spatial_color_feature", which is in the same directory as images folder.
+  # writeMat(paste("./spatial_color_feature/", name, ".mat", sep = ""), scfexport = scf)
 }
-
+save(file="spacial_feature.RData",spacial_feature_all)
 
 
