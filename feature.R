8dfb26be26590255a4e16f0b4f86c16db82f837b
jianitian <jt2867@columbia.edu>
2016-11-02 17:01:52 -0400

    Update feature.R

--- feature.R
+++ feature.R
@@ -1,32 +1,50 @@
 ### Input: dir of the csv file
-### Output: feature 2000*1428
+### Output: feature 2000*196
+#install.packages("randomForest")
+library(randomForest)
+setwd("/Users/jianitian/Desktop/final_project3_1")
+setwd(dir)
+dat_train=read.csv("sift_features_test.csv")
+dat_train=t(dat_train)
 choose_feature=function(dir){
   library(data.table)
   library(dplyr)
   library(rpart)
   library(randomForest)
   
-#setwd("C:/Users/ys2882/Downloads")
+  #setwd("/Users/jianitian/Desktop/project3_final")
+  #dir="/Users/jianitian/Desktop/project3_final"
   setwd(dir)
-  dat_train=fread("sift_features.csv")
   label_train=c(t(data.frame(rep(1,1000))),t(data.frame(rep(0,1000))))
-### PCA on original feature csv
-  pca1=prcomp(t(dat_train))
-  pca1_x=data.frame((pca1$x))
-  var_pca=cumsum((pca1$sdev)^2/sum((pca1$sdev)^2))
-  cutoff=min(which(var_pca>0.8))
-  sub_feature=mutate(pca1_x[,1:cutoff])
-
-### loading RGB feature: rgb_feature_all
+  dat_train=fread("sift_features.csv")
+  
+  
+  ### loading RGB feature: rgb_feature_all
   load("rgb_feature.RData")
   rgb_feature_all=tbl_df(rgb_feature_all)
   #rgb_feature_all=tbl_df(rgb_feature(dir))
-
-### load feature from Xuechun
+  
+  ### load feature from Xuechun
   load("orb_features_500.RData")
-
-### combine the features into 1 final feature
-  feature=data.frame(sub_feature,rgb_feature_all,orb_features_500,label_train)
-  save(file="training_feature.RData",feature)
-  return(feature)
+  orb_features_500=tbl_df(orb_features_500)
+  
+  load("pca1.RData")
+  
+  
+  feature_ori=data.frame(dat_train,rgb_feature_all,orb_features_500)
+  #sub_feature=as.matrix(feature_ori) %*% pca1_rot[,1:196]
+  sub_feature=predict(pca1,newdata=feature_ori)[,1:196]
+  ### PCA on feature csv
+  #pca1=prcomp(feature_ori)
+  #pca1_x=data.frame((pca1$x))
+  #var_pca=cumsum((pca1$sdev)^2/sum((pca1$sdev)^2))
+  #cutoff=min(which(var_pca>0.8))
+  #sub_feature=mutate(pca1_x[,1:cutoff])
+ 
+  
+  ### combine the features into 1 final feature
+  #save(file="feature_196.RData",sub_feature)
+  return(sub_feature)
 }
+fea <- choose_feature("/Users/jianitian/Desktop/project3_final")
+save(fea,file = "feature_196.RData")
